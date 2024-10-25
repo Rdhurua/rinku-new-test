4a244ece8eca2d09c8b2280be7310325f01f940f
Rinku Dhurua <rinkudhurua258@gmail.com>
2024-10-25 14:32:43 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -13,17 +13,18 @@ import appointmentRouter from "./router/appointmentRouter.js";
 const app = express();
 config({path: "./config/config.env"});
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URI, process.env.DASHBORD_URI],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-})
-);
-app.options('*', cors({
-    origin:[process.env.FRONTEND_URI, process.env.DASHBORD_URI],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URI, process.env.DASHBORD_URI],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// })
+// );
+// app.options('*', cors({
+//     origin:[process.env.FRONTEND_URI, process.env.DASHBORD_URI],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// }));
+ app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
